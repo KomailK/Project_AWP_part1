658675e5e5d9d871dcefbb88c9183385bc7f67c1
KomailK <98917001+KomailK@users.noreply.github.com>
2023-12-02 13:38:43 -0500

    Add files via upload

--- data/data.js
+++ data/data.js
@@ -8,7 +8,7 @@ const Products=[
 
     {
         title:"Gaming 4K Monitor",
-        description:"A smooth and responsive display for an amazing visual gaming experience.",
+        description:"A smooth and responsive monitor for an amazing visual gaming experience.",
         image:"https://pixabay.com/fr/photos/jeu-joueur-%C3%A9couteurs-ordinateur-1504466/"
 
     },
@@ -17,6 +17,21 @@ const Products=[
         title:"Gaming Wireless Mouse",
         description:"A ergonomic wireless mouse, perfect for long-time gaming session.",
         image:"https://pixabay.com/fr/photos/souris-rvb-color%C3%A9-ordinateur-7386247/"
+
+    },
+
+    {
+        title:"Gaming Headset",
+        description:"Elevate your gaming with our high-performance headset.",
+        image:"https://pixabay.com/fr/photos/casque-jeu-la-technologie-1227981/"
+
+    },
+
+
+    {
+        title:"Gaming laptop ",
+        description:"Unleash gaming excellence with our powerful gaming laptop.",
+        image:"https://pixabay.com/fr/photos/clavier-ordinateur-la-technologie-5017973/"
 
     }
 
